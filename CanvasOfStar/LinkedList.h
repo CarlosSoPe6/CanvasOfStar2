3d9483c42616828a8556d777e1c435fffc3f0074
Carlos Soto Pérez <carlos348@outlook.com>
2016-11-24 19:00:14 -0600

    nuevo modelo de lista

--- CanvasOfStar/LinkedList.h
+++ CanvasOfStar/LinkedList.h
@@ -2,10 +2,12 @@
 
 typedef struct bitMaps
 {
-	int size;
+	int size_x;
+	int size_y;
 	float x;
 	float y;
-	float speed;
+	float speed_x;
+	float speed_y;
 	float damage;
 	float life;
 	ALLEGRO_BITMAP *image;
@@ -27,35 +29,49 @@ void removeBitMapsListElement(BitMapsList list, BitMap element)
 }
 
 /**
- * param: int size
- * param: float x
- * param: float y
- * param: float speed
- * param float damage;
- * param: float life;
- * param: ALLEGRO_BITMAP * image
- * param: BitMapsList list
- * param BitMap emenent
- * summary: Adds a element from a list
+ * Inits a BitMap
+ * param: size_x
+ * param: size_y
+ * param: x
+ * param: y
+ * param: speed_x
+ * param: speed_y
+ * param: damage
+ * param: life
+ * param: image
+ * param: list
+ * param: element
  */
-void addBitMapsListElemet(int size, float x, float y, float speed, float damage, float life, ALLEGRO_BITMAP * image, BitMapsList * list, BitMap element)
+void addBitMapsListElemet(int size_x, int size_y, float x, float y, float speed_x, float speed_y, float damage, float life, ALLEGRO_BITMAP * image, BitMapsList * list, BitMap element)
 {
 	element.damage = damage;
 	element.image = image;
 	element.life = life;
-	element.size = size;
-	element.speed = speed;
+	element.size_x = size_x;
+	element.size_y = size_y;
 	element.x = x;
 	element.y = y;
-}
+	element.speed_x = speed_x;
+	element.speed_y = speed_y;
+
 	//TODO add "element" to "list"
 }
 
 /**
- * summary: Gets a element from a list
+ * Gets a element from a list
  */
 void getBitMapsListElement()
 {
 	//TODO Create algorithm to get elements
 	
+}
+
+/**
+ * Deletes a element from a list
+ * param: element
+ * param: list
+ */
+void deleteElement(BitMap element, BitMapsList list)
+{
+
 }
